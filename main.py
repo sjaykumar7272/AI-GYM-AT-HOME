3fe1f1e9d59914c2fd2d7c15e3801a066e4c282a
sjaykumar7272 <sjaykumar7272@gmail.com>
2026-06-21 16:04:19 +0530

    create login from ui

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import streamlit as st
+from services.auth.login_wall import render_login_wall
 
-st.set_page_config(page_title="AI GYM Coach")
 
-st.title("🏋️ AI Real-time GYM Coach")
-st.write("Setup successful!")
+def main():
+    st.set_page_config(
+        page_icon="🏋️‍♀️",
+        page_title="AI Real-time GYM Coach",
+        initial_sidebar_state="expanded",
+        layout="centered"
+    )
+
+    if not render_login_wall():
+        return 
+    
+    st.write("Hello")
+
+if __name__ == "__main__":
+    main()
