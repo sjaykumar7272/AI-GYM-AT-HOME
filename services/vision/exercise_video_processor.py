7b9e518e116dcd74ab51437406d924692b6c6017
sjaykumar7272 <sjaykumar7272@gmail.com>
2026-06-21 20:33:03 +0530

    Fix WebRTC TURN server

--- services/vision/exercise_video_processor.py
+++ services/vision/exercise_video_processor.py
@@ -21,7 +21,7 @@ class VideoProcessorClass(VideoProcessorBase):
         self._latest_metrics = None
         self._exercise_type = "Squats"
 
-        model_path = os.path.join(os.getcwd(), "ml_models", "pose_landmarker_full.task")
+        model_path = os.path.join(os.getcwd(), "ml_models", "pose_landmarker.task")
         base_option = python.BaseOptions(model_asset_path=model_path)
 
         options = vision.PoseLandmarkerOptions(
