c498f46985dcabfa45b3a8de352bf2bd10d873ce
sjaykumar7272 <sjaykumar7272@gmail.com>
2026-06-21 16:26:43 +0530

    Metrics Sync + Goal Progress (set completion, workout completion)

--- services/config/workout_config.py
+++ services/config/workout_config.py
@@ -5,3 +5,33 @@ EXERCISE_OPTIONS=[
     "Shoulder Press",
     "Lunges"
 ]
+
+
+
+METRICS_FIELDS = {
+    "Squats": {
+        "knee_angle": 0,
+        "back_angle": 0,
+        "depth_status": "N/A",
+    },
+    "Push-ups": {
+        "elbow_angle": 0,
+        "body_alignment": "N/A",
+        "hip_status": "N/A",
+    },
+    "Biceps Curls (Dumbbell)": {
+        "elbow_angle": 0,
+        "shoulder_status": "N/A",
+        "swing_status": "N/A",
+    },
+    "Shoulder Press": {
+        "elbow_angle": 0,
+        "extension_status": "N/A",
+        "back_arch_status": "N/A",
+    },
+    "Lunges": {
+        "front_knee_angle": 0,
+        "torso_angle": 0,
+        "balance_status": "N/A",
+    },
+}
